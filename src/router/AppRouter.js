f9095cd0a1747b85e4c7d9872d20341aafec3da4
Roy Schrauwen <roy@royschrauwen.nl>
2022-03-08 09:46:22 +0100

    Storing new books in localStorage

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,8 +3,11 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "../components/Header";
 import AddBook from "../components/AddBook";
 import BookList from "../components/BookList";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const AppRouter = () => {
+  const [books, setBooks] = useLocalStorage("books", []);
+
   return (
     <BrowserRouter>
       <div>
@@ -12,7 +15,12 @@ const AppRouter = () => {
         <div className="main-content">
           <Switch>
             <Route component={BookList} path="/" exact={true} />
-            <Route component={AddBook} path="/add" />
+            <Route
+              render={(props) => (
+                <AddBook {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/add"
+            />
           </Switch>
         </div>
       </div>
